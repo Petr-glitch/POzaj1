013c2880f87807f2505e4213c042ddd089a1f343
Petr-glitch <287139@student.pwr.edu.pl>
2025-03-11 18:37:53 +0100

    Add files via upload

---
+++ Lista 1.cpp
@@ -0,0 +1,63 @@
+﻿#include <iostream>
+
+void Z1a(int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        std::cout << i % 10;
+    }
+}
+
+void Z1b(int n)
+{
+    int x = 0;
+    for (int i = 0; i < n; i++)
+    {
+        std::cout << x % 2;
+    }
+}
+void Z1d(int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        std::cout << "0123";
+    }
+}
+void Z1e(int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        std::cout << "0123456789";
+    }
+
+}
+void Z2d(int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        std::cout << std::endl;
+        for (int j = n; j > i; j--)
+        {
+            std::cout << j;
+        }
+        
+    }
+    
+}
+
+int main()
+{
+    int n;
+    std::cin >> n;
+    //Z1a(n);
+    //Z1b(n);
+    //Z1d(n);
+    //Z1e(n);
+    Z2d(n);
+
+    
+
+    return 0;
+}
+
+
